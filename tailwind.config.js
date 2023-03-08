d863043f44acc5d583decb5cf64ce8976786851d
Melih Kamberoğlu <melihkamberoglu19@hotmail.com>
2023-03-08 12:28:03 +0300

    some colors and google fonts has adjusted for project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        lato: ["Lato", "sans-serif"],
+        merriweather: ['Merriweather', 'serif'],
+        gemunu: ['Gemunu Libre', 'sans-serif']
+      },
+
+      colors: {
+        "advo-main": "#2C3639",
+        "advo-purple": "#301E67", 
+        "advo-yellow": "#F6F7C1", 
+        "advo-beige": "#B6EADA", 
+        "advo-blue": "#5B8FB9"
+      }
+    },
   },
   plugins: [],
 }
